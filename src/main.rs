71d4609e2b60ed68d30975393eb4d7f556ea4c15
Pablo COVES <pablo.coves@anatoscope.com>
2022-03-01 11:21:33 +0100

    Add endpoint CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use clap::Parser;
 use cleanup::{args::Args, error::Result};
 use rusoto_ec2::Ec2Client;
+use rusoto_signature::Region;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args: Args = Args::parse();
 
-    let ec2_client = Ec2Client::new(args.region.clone());
+    let region = if let Some(endpoint) = &args.endpoint {
+        Region::Custom {
+            name: args.region.name().to_string(),
+            endpoint: endpoint.to_owned(),
+        }
+    } else {
+        args.region.clone()
+    };
+
+    let ec2_client = Ec2Client::new(region);
 
     cleanup::cleanup(&ec2_client, args).await
 }
